dcd5c703ba7a016a9670ea3cbffd10a4c814a762
Logan-Michel-JF <38558603+Logan-Michel-JF@users.noreply.github.com>
2018-06-18 11:27:25 -0300

    inicio do exemplo JCheckBox

---
+++ exemplo06/ExemploJRadionButton01Principal.java
@@ -0,0 +1,19 @@
+package exemplo06;
+
+import java.awt.EventQueue;
+
+/**
+ * @author Logan Michel
+ */
+public class ExemploJRadionButton01Principal {
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable(){
+        @Override
+        public void run(){
+        new ExemploJRadionButton01();
+            }
+        });
+    }
+    
+}
---
+++ exemplo06/JFrameBaseInterface.java
@@ -0,0 +1,18 @@
+package exemplo06;
+
+/**
+ * @author Logan Michel
+ */
+public interface JFrameBaseInterface {
+    
+    void gerarTela();
+    
+    void adicionarComponentes();
+    
+    void instanciarComponentes();
+    
+    void gerarLocalizacoes();
+    
+    void gerarDimensoes();
+    
+}
